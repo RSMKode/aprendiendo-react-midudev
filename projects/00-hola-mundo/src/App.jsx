b40bcdff53461816641262d04c23cc72e6576a3f
Roger Sancho <rogersanchom@gmail.com>
2023-04-16 19:22:25 +0200

    Commit Boton de Twitter

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
+export function App() {
+  return (
+    <>
+    <TwitterFollowCard userName="RedBurnRoger" name="Roger Sancho"/>
+    <TwitterFollowCard userName="midudev" name="Miguel Ángel Durán"/>
+    <TwitterFollowCard userName="elonmusk" name="Elon Misk"/>
+    </>
+  );
+}
