4850c15149fa0c45248ecccdf78d0c2c7bbea968
Roger <rogersanchom@gmail.com>
2024-04-18 17:40:39 +0200

    Actualizar URL de la API a HTTPS en getMovies.js

--- projects/05-prueba-tecnica-buscador-peliculas/src/services/getMovies.js
+++ projects/05-prueba-tecnica-buscador-peliculas/src/services/getMovies.js
@@ -1,7 +1,7 @@
 const API_KEY = '55569760'
 
-// http://www.omdbapi.com/?i=tt3896198&apikey=55569760&s=''
-const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=`
+// https://www.omdbapi.com/?i=tt3896198&apikey=55569760&s=''
+const API_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=`
 
 export async function getMovies (search) {
   const response = await fetch(API_URL + search)
